5fbca9dc3ade550c6560a8afc1440ddca0b981e2
WenCarmona <wencarmona24@hotmail.com>
2024-08-30 21:18:40 -0500

    2do commit
    creación de formularios y primera parte de las vistas

--- myblogproject/blog/views.py
+++ myblogproject/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Post
+from .forms import PostForm
+from django.db.models import Q
 
 # Create your views here.
+def lista_publicaciones(request):
+    query = request.GET.get('q')
+    fecha = request.GET.get('fecha')
+    categoria = request.GET('categoria')
+
+    posts = Post.objects.all()
+    if query:
+        posts = posts.filter(Q(titulo__incontains=query) | Q(contenido__icontains=query))
+    if fecha:
+        posts = posts.filter(fecha_publicacion=fecha)
+    if categoria:
+        posts = posts.filter(categoria=categoria)
+
+    Paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(page_number)
+
+    return render(request, 'blog/lista_publicaciones.html', {'page_obj': page_obj})
